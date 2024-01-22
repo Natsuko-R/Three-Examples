2fd9e317b21acb71b5a1a79193b19307f4164ee1
skkhexagon <shenjiaojun@gmail.com>
2024-01-22 20:03:31 +0900

    add CameraArray component

--- app/examples/page.tsx
+++ app/examples/page.tsx
@@ -2,9 +2,10 @@ import { AnimationSkinningMorph } from "./components/webgl_animation_skinning_mo
 import { ExporterDRACO } from "./components/misc_exporter_draco";
 import { AmmoRope } from "./components/ammo_rope";
 import { DragControlCanvas } from "./components/misc_controls_drag";
+import { CameraArray } from "./components/webgl_camera_array";
 
 const HousePage = () => {
-	return <AnimationSkinningMorph/>
+	return <CameraArray/>
 };
 
 export default HousePage;
