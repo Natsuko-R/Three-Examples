c8ee30e5dfda352df83fe6753f30011c20528bc0
skkhexagon <shenjiaojun@gmail.com>
2024-02-21 16:40:59 +0900

    update style and add hdr

--- app/examples/components/GLTFViewer.tsx
+++ app/examples/components/GLTFViewer.tsx
@@ -1,13 +1,16 @@
 "use client"
 
 import { useEffect, useRef, useState } from "react"
-import { AmbientLight, AnimationAction, AnimationMixer, CircleGeometry, Clock, DirectionalLight, Mesh, MeshStandardMaterial, Object3D, PerspectiveCamera, Scene, TextureLoader, WebGLCubeRenderTarget, WebGLRenderer, DoubleSide, HemisphereLight, Group } from "three"
+import { EquirectangularReflectionMapping, AmbientLight, AnimationAction, AnimationMixer, CircleGeometry, Clock, DirectionalLight, Mesh, MeshStandardMaterial, Object3D, PerspectiveCamera, Scene, TextureLoader, WebGLCubeRenderTarget, WebGLRenderer, DoubleSide, HemisphereLight, Group, Box3, Vector3 } from "three"
 import { GUI } from "three/examples/jsm/libs/lil-gui.module.min.js"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import Stats from 'three/addons/libs/stats.module.js';
 
-const SKYBOX_PATH_TEXTURE: string = '/images/sky_01.jpg';
-const GROUND_PATH_TEXTURE: string = '/images/moon.jpg';
+const SKYBOX_TEXTURE: string = '/images/sky_01.jpg';
+const HDR_PATH_TEXTURE: string = '/images/pedestrian_overpass_1k.hdr';
+const GROUND_TEXTURE: string = '/images/moon.jpg';
 const INITIAL_MODEL: string = 'models/gltf/RobotExpressive/RobotExpressive.glb';
 
 export const GLTFViewer = () => {
@@ -16,11 +19,12 @@ export const GLTFViewer = () => {
   const sceneRef = useRef<Scene>();
   const rendererRef = useRef<WebGLRenderer>();
 
-  // const [isMount, setIsMount] = useState(false)
+  const [isMount, setIsMount] = useState(false)
+
+  const textureLoader = new TextureLoader();
 
   let scene: Scene, camera: PerspectiveCamera, renderer: WebGLRenderer;
-  let controls: OrbitControls, stats: Stats;
-  let clock: Clock, mixer: AnimationMixer, actions: { [key: string]: AnimationAction }, activeAction: AnimationAction;
+  let controls: OrbitControls, stats: Stats, gui: GUI;
   let model: Object3D;
   let requestId: number;
 
@@ -35,14 +39,13 @@ export const GLTFViewer = () => {
     const dirLight = new DirectionalLight(0xffffff, 3);
     dirLight.position.set(1, 1, 1);
     scene.add(dirLight);
-
     scene.add(new AmbientLight(0xffffff, 3));
 
     // カメラ
 
-    camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.set(400, 200, 0);
-    camera.lookAt(0, 0, 0);
+    camera = new PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 2000);
+    camera.position.set(- 50, 30, 100);
+    camera.lookAt(1000, 1000, 1000);
 
     // レンダラーの作成
 
@@ -56,43 +59,48 @@ export const GLTFViewer = () => {
     // カメラコントローラ
 
     controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true;
-    controls.dampingFactor = 0.05;
-    controls.screenSpacePanning = false;
-    controls.minDistance = 100;
-    controls.maxDistance = 500;
-    controls.maxPolarAngle = Math.PI / 2;
-    controls.update();
+    // controls.enableDamping = true;
+    // controls.dampingFactor = 0.05;
+    // controls.screenSpacePanning = false;
+    // controls.minDistance = 100;
+    // controls.maxDistance = 500;
+    // controls.maxPolarAngle = Math.PI / 2;
+    // controls.update();
 
     // スカイボックス
 
-    const textureLoader = new TextureLoader();
-    textureLoader.load(SKYBOX_PATH_TEXTURE, (texture) => {
-      const target = new WebGLCubeRenderTarget(texture.image.height);
-      target.fromEquirectangularTexture(renderer, texture);
-      scene.background = target.texture;
-      console.log("SkyBox : succeeded on loading " + SKYBOX_PATH_TEXTURE);
-    }, undefined, () => {
-      console.error("SkyBox : failed on loading " + SKYBOX_PATH_TEXTURE);
-      return;
+    // textureLoader.load(SKYBOX_TEXTURE, (texture) => {
+    //   const target = new WebGLCubeRenderTarget(texture.image.height);
+    //   target.fromEquirectangularTexture(renderer, texture);
+    //   scene.background = target.texture;
+    //   console.log("SkyBox : succeeded on loading " + SKYBOX_TEXTURE);
+    // }, undefined, () => {
+    //   console.error("SkyBox : failed on loading " + SKYBOX_TEXTURE);
+    //   return;
+    // });
+
+    new RGBELoader().load(HDR_PATH_TEXTURE, function (texture) {
+      texture.mapping = EquirectangularReflectionMapping;
+      scene.background = texture;
+      scene.environment = texture;
     });
 
     // 地面
 
-    const groundTexture = textureLoader.load(GROUND_PATH_TEXTURE, () => {
-      console.log("Ground : succeeded on loading " + GROUND_PATH_TEXTURE);
-    }, undefined, () => {
-      console.log("Ground : failed on loading " + GROUND_PATH_TEXTURE);
-      return;
-    });
-    const groundMaterial = new MeshStandardMaterial({ side: DoubleSide });
-    groundMaterial.map = groundTexture;
-    const groundGeometry = new CircleGeometry(300, 300);
-    const groundModel = new Mesh(groundGeometry, groundMaterial);
-    groundModel.name = "ground";
-    groundModel.position.set(0, 0, 0);
-    groundModel.rotation.x = (-0.5 * Math.PI);
-    scene.add(groundModel);
+    // const groundTexture = textureLoader.load(GROUND_TEXTURE, () => {
+    //   console.log("Ground : succeeded on loading " + GROUND_TEXTURE);
+    // }, undefined, () => {
+    //   console.log("Ground : failed on loading " + GROUND_TEXTURE);
+    //   return;
+    // });
+    // const groundMaterial = new MeshStandardMaterial({ side: DoubleSide });
+    // groundMaterial.map = groundTexture;
+    // const groundGeometry = new CircleGeometry(300, 300);
+    // const groundModel = new Mesh(groundGeometry, groundMaterial);
+    // groundModel.name = "ground";
+    // groundModel.position.set(0, 0, 0);
+    // groundModel.rotation.x = (-0.5 * Math.PI);
+    // scene.add(groundModel);
 
     // initial model
 
@@ -100,8 +108,7 @@ export const GLTFViewer = () => {
     loader.load(INITIAL_MODEL, function (gltf) {
       model = gltf.scene;
       model.name = "InitialModel";
-      model.scale.set(40, 40, 40);
-
+      model.scale.set(3, 3, 3);
       scene.add(model);
     }, undefined, function (e) {
       console.error(e);
@@ -109,13 +116,32 @@ export const GLTFViewer = () => {
 
     // dat.gui.
 
-    const gui = new GUI();
+    gui = new GUI();
     gui.add(camera, "fov")
       .min(10)
       .max(100)
       .step(1)
       .onChange(() => camera.updateProjectionMatrix())
       .name("カメラ調整")
+
+    const positionControls = gui.addFolder('Position');
+    positionControls.add(camera.position, 'x', -500, 500)
+      .name('Position X')
+      .step(1)
+      .onChange(() => camera.updateProjectionMatrix());
+    positionControls.add(camera.position, 'y', -500, 500)
+      .name('Position Y')
+      .step(1)
+      .onChange(() => camera.updateProjectionMatrix());
+    positionControls.add(camera.position, 'z', -500, 500)
+      .name('Position Z')
+      .step(1)
+      .onChange(() => camera.updateProjectionMatrix());
+
+    // stats
+
+    stats = new Stats();
+    document.querySelector("#container")?.appendChild(stats.dom);
 
     // init refs
 
@@ -127,6 +153,13 @@ export const GLTFViewer = () => {
   const animate = () => {
     renderer.render(scene, camera);
     controls.update();
+
+    scene.children.forEach(item => {
+      if (item.name === "NewModel" || item.name === "InitialModel") {
+        item.rotation.y += 0.01;
+      }
+    });
+
     requestId = requestAnimationFrame(animate);
   }
 
@@ -140,7 +173,7 @@ export const GLTFViewer = () => {
     if (!sceneRef.current || !rendererRef.current) return;
 
     const scene = sceneRef.current;
-    const groups = scene.children.filter(item => 
+    const groups = scene.children.filter(item =>
       item.parent && (item.name === "NewModel" || item.name === "InitialModel")
     );
     groups.forEach((g) => g.parent?.remove(g));
@@ -155,34 +188,43 @@ export const GLTFViewer = () => {
 
     const loader = new GLTFLoader();
     loader.load(URL.createObjectURL(file), (gltf) => {
+
+      const boundingBox = new Box3().setFromObject(gltf.scene);
+      const size = new Vector3();
+      boundingBox.getSize(size);
+      const desiredSize = window.innerHeight * (1 / 4);
+
       const model = gltf.scene;
-      model.name = "NewModel"
-      model.position.set(0, 20, 0);
+      model.name = "NewModel";
+      model.scale.set(desiredSize / size.x, desiredSize / size.y, desiredSize / size.z);
+
       scene.add(model);
-    });
+    }, undefined, (e) => console.error(e));
+    rendererRef.current?.render(scene, camera);
   };
 
   useEffect(() => {
-    // if (isMount) {
-    initWorld();
-    animate();
-    window.addEventListener("resize", onWindowResize) // リサイズ処理
-    // };
-    // setIsMount(true);
+    if (isMount) {
+      initWorld();
+      animate();
+      window.addEventListener("resize", onWindowResize) // リサイズ処理
+    };
+    setIsMount(true);
     return () => {
       cancelAnimationFrame(requestId)
       window.removeEventListener("resize", onWindowResize)
-      scene.remove();
-      renderer.dispose();
+      gui?.destroy();
+      sceneRef.current?.remove();
+      rendererRef.current?.dispose();
     }
-    // }, [isMount])
-  }, [])
+  }, [isMount])
+  // }, [])
 
-  // if (!isMount) return null
+  if (!isMount) return null
 
   return (
-    <div className="relative" >
-      <div className="absolute top-4 left-4 z-50" >
+    <div id="container" className="relative flex justify-center">
+      <div id="input" className="absolute bottom-[4rem] z-50">
         <input
           type="file"
           accept=".gltf,.glb"
@@ -190,8 +232,8 @@ export const GLTFViewer = () => {
           onChange={handleFileUpload}
           className="hidden"
         />
-        <button onClick={() => fileInputRef.current?.click()} className="bg-slate-500 hover:bg-slate-600 text-white font-bold py-2 px-4 rounded">
-          Import Local Model
+        <button onClick={() => fileInputRef.current?.click()} className="bg-slate-500 hover:bg-slate-600 text-white font-bold py-3 px-6 rounded">
+          Upload Your GLTF Model !
         </button>
       </div>
       <canvas id="glcanvas" className="relative" ref={canvasRef} />
